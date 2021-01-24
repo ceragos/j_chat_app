a578f558c0421a53bbbc5bb53a5fa2031de84baa
ceragos <antonio.casta_yo@hotmail.com>
2021-01-23 23:39:19 -0500

    Se crea vista para listar usuarios conectados al chat

---
+++ apps/chat/views/chat.py
@@ -0,0 +1,22 @@
+from django.views.generic import TemplateView
+
+from apps.chat.models import Message
+from apps.usuarios.models import User
+
+
+class ChatTemplateView(TemplateView):
+    template_name = 'chat/chat.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ChatTemplateView, self).get_context_data()
+        context['users'] = self.get_all_users_on_line()
+        context['messages'] = self.get_all_messages()
+        return context
+
+    def get_all_users_on_line(self):
+        users = User.objects.filter(is_online=True).exclude(pk=self.request.user.pk)
+        return users
+
+    def get_all_messages(self):
+        users = Message.objects.all()
+        return users
